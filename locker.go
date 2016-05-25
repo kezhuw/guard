930b3321946311bd805b27fc4962fb305316b852
Kezhu Wang <kezhuw@gmail.com>
2016-05-25 16:53:47 +0800

    Add more document for guard.Locker

--- locker.go
+++ locker.go
@@ -1,11 +1,13 @@
 package guard
 
 // Locker is a one-time sync.Locker, with additional Release method
-// to abandon itself in initial state.
+// to abandon itself in initial state. It is designed for single
+// goroutine usage.
 type Locker interface {
 	Lock()
 	Unlock()
 
 	// Release abandons locker, can only be called in initial state.
+	// It starts a new goroutine to do Lock() and Unlock().
 	Release()
 }
