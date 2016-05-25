89edb7f1ecc78ab5b3f1590e2475d02ac8e35294
Kezhu Wang <kezhuw@gmail.com>
2016-05-25 16:49:21 +0800

    Panic in ill usage with more information

--- guard.go
+++ guard.go
@@ -2,45 +2,81 @@ package guard
 
 import "sync"
 
+const (
+	errLockerLocked    = "guard.Guard: locker is already locked"
+	errLockerUnlocked  = "guard.Guard: locker is already unlocked or released"
+	errLockerNotLocked = "guard.Guard: locker is not locked"
+)
+
 type locker struct {
 	g *Guard
-	c chan struct{}
+	c chan *Guard
 }
 
 func (l *locker) Lock() {
-	if _, ok := <-l.c; !ok {
-		panic("guard: duplicated locking")
+	c := l.c
+	if c == nil {
+		panic(errLockerUnlocked)
 	}
+	g, ok := <-c
+	if !ok {
+		panic(errLockerLocked)
+	}
+	l.g = g
 }
 
 func (l *locker) Unlock() {
-	l.g.unlock(l.c)
+	c := l.c
+	switch {
+	case c == nil:
+		panic(errLockerUnlocked)
+	case l.g == nil:
+		panic(errLockerNotLocked)
+	default:
+		l.g.unlock(c)
+		l.g = nil
+		l.c = nil
+	}
 }
 
 func (l *locker) Release() {
-	go func() {
-		l.Lock()
-		l.Unlock()
-	}()
+	c := l.c
+	switch {
+	case c == nil:
+		panic(errLockerUnlocked)
+	case l.g != nil:
+		panic(errLockerLocked)
+	default:
+		go releaseLocker(c)
+		l.c = nil
+	}
+}
+
+func releaseLocker(c chan *Guard) {
+	g, ok := <-c
+	if !ok {
+		panic("guard.Guard: locker is locked in releasing")
+	}
+	g.unlock(c)
 }
 
 // Guard grants exclusive access permission to clients.
 type Guard struct {
 	mu    sync.Mutex
 	off   int
-	locks []chan struct{}
+	locks []chan *Guard
 }
 
-func unblock(c chan struct{}) {
-	c <- struct{}{}
+func unblockGuardLocker(c chan *Guard, g *Guard) {
+	c <- g
 	close(c)
 }
 
-func (g *Guard) unlock(c chan struct{}) {
+func (g *Guard) unlock(c chan *Guard) {
 	g.mu.Lock()
 	l := len(g.locks)
 	if g.off == l || g.locks[g.off] != c {
-		panic("guard.Guard: unlocking unlocked")
+		panic(errLockerNotLocked)
 	}
 	g.locks[g.off] = nil
 	g.off++
@@ -49,7 +85,7 @@ func (g *Guard) unlock(c chan struct{}) {
 		g.off = 0
 		g.locks = g.locks[:0]
 	default:
-		unblock(g.locks[g.off])
+		unblockGuardLocker(g.locks[g.off], g)
 		if g.off >= l/2 || g.off >= 32 {
 			copy(g.locks, g.locks[g.off:l])
 			n := l - g.off
@@ -61,7 +97,7 @@ func (g *Guard) unlock(c chan struct{}) {
 	g.mu.Unlock()
 }
 
-func collectLockers(lockers []chan struct{}) {
+func collectLockers(lockers []chan *Guard) {
 	for i, n := 0, len(lockers); i < n; i++ {
 		lockers[i] = nil
 	}
@@ -71,13 +107,13 @@ func collectLockers(lockers []chan struct{}) {
 // Lockers created after will not acquire the permission before this one got
 // locked/unlocked or released.
 func (g *Guard) NewLocker() Locker {
-	c := make(chan struct{}, 1)
+	c := make(chan *Guard, 1)
 	g.mu.Lock()
 	owned := g.off == len(g.locks)
 	g.locks = append(g.locks, c)
 	g.mu.Unlock()
 	if owned {
-		unblock(c)
+		unblockGuardLocker(c, g)
 	}
-	return &locker{g: g, c: c}
+	return &locker{c: c}
 }
--- rwguard.go
+++ rwguard.go
@@ -2,52 +2,122 @@ package guard
 
 import "sync"
 
+const (
+	errReaderLocked    = "guard.RWGuard: reader is already locked"
+	errReaderUnlocked  = "guard.RWGuard: reader is already unlocked or released"
+	errReaderNotLocked = "guard.RWGuard: reader is not locked"
+
+	errWriterLocked    = "guard.RWGuard: writer is already locked"
+	errWriterUnlocked  = "guard.RWGuard: writer is already unlocked or released"
+	errWriterNotLocked = "guard.RWGuard: writer is not locked"
+)
+
 type reader struct {
 	g *RWGuard
-	c chan struct{}
+	c chan *RWGuard
 }
 
 func (r *reader) Lock() {
-	if _, ok := <-r.c; !ok {
-		panic("guard.RWGuard: duplicated read locking")
+	c := r.c
+	if c == nil {
+		panic(errReaderUnlocked)
 	}
+	g, ok := <-c
+	if !ok {
+		panic(errReaderLocked)
+	}
+	r.g = g
 }
 
 func (r *reader) Unlock() {
-	r.g.unlockRead(r.c)
+	c := r.c
+	switch {
+	case c == nil:
+		panic(errReaderUnlocked)
+	case r.g == nil:
+		panic(errReaderNotLocked)
+	default:
+		r.g.unlockRead(c)
+		r.g = nil
+		r.c = nil
+	}
 }
 
 func (r *reader) Release() {
-	go func() {
-		r.Lock()
-		r.Unlock()
-	}()
+	c := r.c
+	switch {
+	case c == nil:
+		panic(errReaderUnlocked)
+	case r.g != nil:
+		panic(errReaderLocked)
+	default:
+		go releaseRead(c)
+		r.c = nil
+	}
+}
+
+func releaseRead(c chan *RWGuard) {
+	g, ok := <-c
+	if !ok {
+		panic("guard.RWGuard: reader is locked in releasing")
+	}
+	g.unlockRead(c)
 }
 
 type writer struct {
 	g *RWGuard
-	c chan struct{}
+	c chan *RWGuard
 }
 
 func (w *writer) Lock() {
-	if _, ok := <-w.c; !ok {
-		panic("guard.RWGuard: duplicated write locking")
+	c := w.c
+	if c == nil {
+		panic(errWriterUnlocked)
 	}
+	g, ok := <-c
+	if !ok {
+		panic(errWriterLocked)
+	}
+	w.g = g
 }
 
 func (w *writer) Unlock() {
-	w.g.unlockWrite(w.c)
+	c := w.c
+	switch {
+	case c == nil:
+		panic(errWriterUnlocked)
+	case w.g == nil:
+		panic(errWriterNotLocked)
+	default:
+		w.g.unlockWrite(c)
+		w.g = nil
+		w.c = nil
+	}
 }
 
 func (w *writer) Release() {
-	go func() {
-		w.Lock()
-		w.Unlock()
-	}()
+	c := w.c
+	switch {
+	case c == nil:
+		panic(errWriterUnlocked)
+	case w.g != nil:
+		panic(errWriterLocked)
+	default:
+		go releaseWrite(c)
+		w.c = nil
+	}
+}
+
+func releaseWrite(c chan *RWGuard) {
+	g, ok := <-c
+	if !ok {
+		panic("guard.RWGuard: writer is locked in releasing")
+	}
+	g.unlockWrite(c)
 }
 
 type waiter struct {
-	c       chan struct{}
+	c       chan *RWGuard
 	writing bool
 }
 
@@ -55,28 +125,33 @@ type waiter struct {
 type RWGuard struct {
 	mu      sync.Mutex
 	off     int
-	readers map[chan struct{}]struct{}
+	readers map[chan *RWGuard]struct{}
 	waiters []waiter
 }
 
-func (g *RWGuard) unlockRead(c chan struct{}) {
+func unblockRWGuardLocker(c chan *RWGuard, g *RWGuard) {
+	c <- g
+	close(c)
+}
+
+func (g *RWGuard) unlockRead(c chan *RWGuard) {
 	g.mu.Lock()
 	defer g.mu.Unlock()
 	if _, ok := g.readers[c]; !ok {
-		panic("guard.RWGuard: unlocking unlocked reader")
+		panic(errReaderNotLocked)
 	}
 	delete(g.readers, c)
 	if len(g.readers) == 0 && g.off != len(g.waiters) {
-		unblock(g.waiters[g.off].c)
+		unblockRWGuardLocker(g.waiters[g.off].c, g)
 	}
 }
 
-func (g *RWGuard) unlockWrite(c chan struct{}) {
+func (g *RWGuard) unlockWrite(c chan *RWGuard) {
 	g.mu.Lock()
 	defer g.mu.Unlock()
 	i, l := g.off, len(g.waiters)
 	if len(g.readers) != 0 || i == l || g.waiters[i].c != c {
-		panic("guard.RWGuard: unlocking unlocked writer")
+		panic(errWriterNotLocked)
 	}
 	g.waiters[i].c = nil
 	i++
@@ -88,9 +163,9 @@ func (g *RWGuard) unlockWrite(c chan struct{}) {
 		c := g.waiters[i].c
 		switch g.waiters[i].writing {
 		case true:
-			unblock(c)
+			unblockRWGuardLocker(c, g)
 		default:
-			unblock(c)
+			unblockRWGuardLocker(c, g)
 			g.waiters[i].c = nil
 			g.readers[c] = struct{}{}
 			i++
@@ -99,7 +174,7 @@ func (g *RWGuard) unlockWrite(c chan struct{}) {
 					break
 				}
 				c := g.waiters[i].c
-				unblock(c)
+				unblockRWGuardLocker(c, g)
 				g.waiters[i].c = nil
 				g.readers[c] = struct{}{}
 			}
@@ -132,34 +207,34 @@ func collectWaiters(waiters []waiter) {
 // Writers created after will not acquire their permission before this one got
 // locked/unlocked or released.
 func (g *RWGuard) NewReader() Locker {
-	c := make(chan struct{}, 1)
+	c := make(chan *RWGuard, 1)
 	g.mu.Lock()
 	if g.readers == nil {
-		g.readers = make(map[chan struct{}]struct{})
+		g.readers = make(map[chan *RWGuard]struct{})
 	}
 	switch {
 	case g.off == len(g.waiters):
 		g.readers[c] = struct{}{}
 		g.mu.Unlock()
-		unblock(c)
+		unblockRWGuardLocker(c, g)
 	default:
 		g.waiters = append(g.waiters, waiter{c: c})
 		g.mu.Unlock()
 	}
-	return &reader{g: g, c: c}
+	return &reader{c: c}
 }
 
 // NewWriter creates a Locker for write permission acquisition.
 // Lockers, including readers and writers, will not acquire their permission
 // before this one got locked/unlocked or released.
 func (g *RWGuard) NewWriter() Locker {
-	c := make(chan struct{}, 1)
+	c := make(chan *RWGuard, 1)
 	g.mu.Lock()
 	owned := len(g.readers) == 0 && len(g.waiters) == 0
 	g.waiters = append(g.waiters, waiter{c: c, writing: true})
 	g.mu.Unlock()
 	if owned {
-		unblock(c)
+		unblockRWGuardLocker(c, g)
 	}
-	return &writer{g: g, c: c}
+	return &writer{c: c}
 }
